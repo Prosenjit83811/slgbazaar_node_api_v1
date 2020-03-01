005a992ad70e38efef49fef95c546b460c9e3012
Prosenjit Sarkar <bestprosenjit@gmail.com>
2020-03-01 22:33:38 +0530

    show profile

--- Api/v1.0/Controllers/User/ProfileController.js
+++ Api/v1.0/Controllers/User/ProfileController.js
@@ -3,5 +3,31 @@ const User = require('../../Models/UserModel')
 const dbConfig = require('../../Config/DB');
 
 exports.index = (req, res) => {
-    return  res.json("work");
+    console.log(req.user);
+    var userId = req.user.id;
+   
+    try {
+        User.findById(userId)
+            .exec()
+            .then(result => {
+                if(result){
+                    res.status(200).json({
+                        data: result
+                    });
+                }else{
+                    res.status(404).json({
+                        message: "Not found any record"
+                    });
+                }
+                
+            })
+            .catch(error => {
+                res.status(500).json({
+                    error: error
+                });
+            });
+    
+    } catch (error) {
+        res.send(500);
+    }
 }
