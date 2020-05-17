faeb1cded617cd4a84e564539c2e8b855f6ef570
Prosenjit Sarkar <bestprosenjit@gmail.com>
2020-05-17 21:07:09 +0530

    add help.js file

---
+++ help.js
@@ -0,0 +1,5 @@
+// Server API URL -> http://localhost:3000/api/1.0/auth/login
+// Start Server -> nodemon start
+// Stop Server -> pkill -f node
+// Migration -> node Seedes
+// MongoDB graphical interface with MongoDB Compass
