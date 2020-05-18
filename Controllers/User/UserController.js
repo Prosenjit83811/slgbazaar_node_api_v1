1e65097f567a922e91d2012600955b3276c00ef7
Prosenjit Sarkar <bestprosenjit@gmail.com>
2020-05-18 22:10:19 +0530

    add user logic

--- Controllers/User/UserController.js
+++ Controllers/User/UserController.js
@@ -4,8 +4,45 @@ const Role = require('../../Models/RoleModel')
 const dbConfig = require('../../Config/DB');
 
 
-exports.store = (req, res) => {
+exports.store = async (req, res) => {
 
-    return  res.json({"msg":"Successfully Added"});
+    try {
+        let id = "";
+        const user = new User(req.body);
+        await user.save((error, result)=>{
+            if(error) {
+                console.log(error);
+                res.status(500);
+                res.send(error);
+            }
+            else {
+                id = result._id;
+                res.status(200);
+                res.json({
+                    message: "Successfully Added"
+                });
+            }
+        });
+
+        // const user = new User(req.body);
+        // await user.save((error, result)=>{
+        //     if(error) {
+        //         console.log(error);
+        //         res.status(500);
+        //         res.send(error);
+        //     }
+        //     else {
+        //         id = result._id;
+        //         res.status(200);
+        //         res.json({
+        //             message: "Successfully Added"
+        //         });
+        //     }
+        // });
+
+    } catch (error) {
+        res.send(500);
+    }
+
 
 }
--- Validations/UserValidation.js
+++ Validations/UserValidation.js
@@ -4,13 +4,22 @@ const User = require('../Models/UserModel')
 
 exports.user =  [
 
-    check('number')
+    check('number').custom(value => {
+        return User.findUserByNumber(value).then(user => {
+          if (user) {
+            return Promise.reject('This Number allredy used');
+          }
+        });
+    })
     .notEmpty().withMessage('Number not empty')
-    .isInt().withMessage('invalied Nnmber').isLength({ min: 10 }).withMessage('Number minimam 10'),
+    .isInt().withMessage('invalied number').isLength({ min: 10 }).withMessage('Number minimam 10'),
 
     check('password')
     .notEmpty().withMessage('Password not empty')
     .isLength({ min: 6 }).withMessage('password minimam 6'),
+
+    check('role')
+    .notEmpty().withMessage('Role not empty'),
 
     function(req,res,next) {
 
