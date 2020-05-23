540c9f160a7c7d35aafb52acc0d7b17ed5b8860b
Prosenjit Sarkar <bestprosenjit@gmail.com>
2020-05-23 22:29:21 +0530

    install mongoose-paginate-v2, category model add pegination, category populate with ppagination, and category add condaction

--- Controllers/Category/CategoryController.js
+++ Controllers/Category/CategoryController.js
@@ -1,13 +1,28 @@
 // const jwt = require('jsonwebtoken');
 const Category = require('../../Models/CategoryModel')
+// const mongoosePaginate = require('mongoose-paginate-v2');
 // const Role = require('../../Models/RoleModel')
 // const dbConfig = require('../../Config/DB');
 
 exports.index = (req, res) => {
     
     try {
-        Category.find().populate("sub_categories")
-            .exec()
+        const options = {
+            page: 1,
+            limit: 2,
+            populate: {
+                path: 'sub_categories', 
+                match: { is_deleted: false }
+            }
+        };
+
+        var query   = {
+            is_deleted: false 
+        };
+
+        Category.paginate(query, options)
+        // .populate("sub_categories")
+            // .exec()
             .then(result => {
                 if(result){
                     res.status(200).json({
--- Models/CategoryModel.js
+++ Models/CategoryModel.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate-v2');
 
 var categorySchema = mongoose.Schema({
     category: { type: String, required: true},
     description: { type: String, required: true},
+    is_deleted: { type: Boolean, 'default': false },
+    created_at: { type: Date, 'default': Date.now },
+    updated_at: { type: Date, 'default': Date.now },
     sub_categories: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: "Category"
     }]
 });
-
+categorySchema.plugin(mongoosePaginate);
 const Category = module.exports = mongoose.model('Category', categorySchema);
