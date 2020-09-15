c29aa5f1f49d32bc5188b95422209ab4856c1a4d
Papai Sarkar <iampapaisarkar@gmail.com>
2020-09-16 00:17:34 +0530

    add product crud oprations

--- Controllers/Dashboard/Product/ProductController.js
+++ Controllers/Dashboard/Product/ProductController.js
@@ -3,11 +3,19 @@ require('../../../Models/ProductVariationModel')
 require('../../../Models/ProductAttributeModel')
 
 exports.index = (req, res) => {
+
+    let sortKey = req.query.sort_by;
+    let sort = {};
+    if(req.query.sort == 'asc'){
+        sort[sortKey] = 1;
+    }else{
+        sort[sortKey] = -1;
+    }
     
         const options = {
-            sort: {"name": -1},
-            page: 1,
-            limit: 2,
+            sort: sort,
+            page: req.query.page,
+            limit: req.query.limit,
             populate: [{
                 select: ['_id','variation'],
                 path: 'productVariations', 
@@ -49,4 +57,108 @@ exports.index = (req, res) => {
                 });
             });
     
+}
+
+exports.show = (req, res) => {
+    
+    var id = req.params.id;
+   
+    try {
+        Product.findById(id)
+            .find({isDeleted: false})
+            .exec()
+            .then(result => {
+                if(result){
+                    res.status(200).json({
+                        data: result
+                    });
+                }else{
+                    res.status(404).json({
+                        message: "Not found any record"
+                    });
+                }
+                
+            })
+            .catch(error => {
+                res.status(500).json({
+                    error: error
+                });
+            });
+    
+    } catch (error) {
+        res.send(500);
+    }
+
+}
+
+
+exports.store = async (req, res) => {
+
+    try {
+        const product = new Product(req.body);
+
+        await product.save((error, result)=>{
+            if(error) {
+                console.log(error);
+                res.status(500);
+                res.send(error);
+            }
+            else {
+                res.status(200);
+                res.json({
+                    message: "Successfully Added"
+                });
+            }
+        });
+
+    } catch (error) {
+        res.send(500);
+    }
+
+
+}
+
+
+exports.update = async (req, res) => {
+
+    try {
+        Product.findByIdAndUpdate(req.params.id, req.body, function(error, result){
+            if(error) {
+                res.status(500);
+                res.send(error);
+            }
+            else {
+                res.status(200);
+                res.json({
+                    message: "Successfully Updated"
+                });
+            }
+        });
+
+    } catch (error) {
+        res.send(500);
+    }
+
+}
+
+exports.delete = async (req, res) => {
+
+    try {
+        Product.findByIdAndUpdate(req.params.id, {isDeleted:true}, function(error, result){
+            if(error) {
+                res.status(500);
+                res.send(error);
+            }
+            else {
+                res.status(200);
+                res.json({
+                    message: "Successfully Deleted"
+                });
+            }
+        });
+
+    } catch (error) {
+        res.send(500);
+    }
+
 }
--- Routes/ProductRoute.js
+++ Routes/ProductRoute.js
@@ -7,6 +7,9 @@ const ProductValidation = require('../Validations/ProductValidation');
 
 // router.post('/', CategoryValidation.category, CategoryController.store);
 router.get('/', ProductController.index);
-router.post('/',  ProductValidation.product, ProductController.index);
+router.get('/:id', ProductController.show);
+router.post('/',  ProductValidation.product, ProductController.store);
+router.put('/:id', ProductValidation.product, ProductController.update);
+router.delete('/:id', ProductController.delete);
 
 module.exports = router;
