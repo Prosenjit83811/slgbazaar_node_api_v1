b4f5c9100875f742919f24316318dfebaeab9375
Your Name <you@example.com>
2021-06-02 23:22:16 +0530

    category select change key name

--- Controllers/Dashboard/Category/CategoryController.js
+++ Controllers/Dashboard/Category/CategoryController.js
@@ -2,34 +2,91 @@ const Category = require('../../../Models/CategoryModel')
 
 exports.index = (req, res) => {
     
+    // try {
+    //     let sortKey = req.query.sort_by;
+    //     let sort = {};
+    //     if(req.query.sort == 'asc'){
+    //         sort[sortKey] = 1;
+    //     }else{
+    //         sort[sortKey] = -1;
+    //     }
+    //     const options = {
+    //         sort: sort,
+    //         page: req.query.page,
+    //         limit: req.query.limit,
+    //         populate: {
+    //             path: 'sub_categories', 
+    //             match: { isDeleted: false }
+    //         }
+    //     };
+
+    //     var query   = {
+    //         isDeleted: false 
+    //     };
+
+    //     Category.paginate(query, options)
+    //         .then(result => {
+    //             if(result){
+    //                 res.status(200).json({
+    //                     data: result
+    //                 });
+    //             }else{
+    //                 res.status(404).json({
+    //                     message: "Not found any record"
+    //                 });
+    //             }
+                
+    //         })
+    //         .catch(error => {
+    //             res.status(500).json({
+    //                 error: error
+    //             });
+    //         });
+    
+    // } catch (error) {
+    //     res.send(500);
+    // }
+
+    // Category.updateMany( {}, { $rename: { 'category': 'sdsdf' } }, {multi: true} )
+
+
+    // Category.aggregate([
+    //     { $project:
+    //         {
+    //             name: '$category',
+    //             isDeleted: '$isDeleted',
+    //         }
+    // },
+    // { "$match": { "isDeleted": false } },
+    //   ], function(err, contacts) {
+    //     console.log('contacts', contacts)
+    //     contacts.populate(result, {path: "sub_categories"}, callback);
+    //   });
+
     try {
-        let sortKey = req.query.sort_by;
-        let sort = {};
-        if(req.query.sort == 'asc'){
-            sort[sortKey] = 1;
-        }else{
-            sort[sortKey] = -1;
-        }
-        const options = {
-            sort: sort,
-            page: req.query.page,
-            limit: req.query.limit,
-            populate: {
-                path: 'sub_categories', 
-                match: { isDeleted: false }
-            }
-        };
-
-        var query   = {
-            isDeleted: false 
-        };
-
-        Category.paginate(query, options)
+        Category
+            .find({isDeleted: false})
+            .select({name: '$category'})
+            .populate({
+                path: 'sub_categories',
+                match: { isDeleted: false },
+                select: {name: '$category'}
+            })
+            // .aggregate([
+            //     { $project:
+            //             {
+            //                 name: '$category',
+            //                 isDeleted: '$isDeleted',
+            //             }
+            //     },
+            //     { "$match": { "isDeleted": false } },
+            // ])
+            .exec()
             .then(result => {
                 if(result){
-                    res.status(200).json({
-                        data: result
-                    });
+                    res.status(200).json(
+                        result
+                    );
                 }else{
                     res.status(404).json({
                         message: "Not found any record"
